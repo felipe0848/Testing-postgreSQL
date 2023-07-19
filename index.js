d2939c1af9c35ebdecf6ed0d03a4f9e58dfed0e0
Felipe Soares <felipe.11.11@hotmail.com>
2023-07-19 12:21:11 -0400

    Delete Planet

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 (async () => {
     const Planet = require("./models/Planet");
 
-    //   const newPlanet = await Planet.create({
-    //     name: "Venus",
-    //     position: 5,
-    //   });
+    const newPlanet = await Planet.create({
+        name: "Venus",
+        position: 5,
+    });
 
-    // const seePlanet = await Planet.findAll({
-    // where: {
-    //     position: 3,
-    // },
-    // });
+    const seePlanet = await Planet.findAll();
 
-    const updatePlanet = await Planet.findByPk(1);
-    updatePlanet.name = "terra";
-    await updatePlanet.save();
+    console.log(seePlanet.map((p) => p.dataValues));
 
-    console.log(updatePlanet);
+    // const updatePlanet = await Planet.findByPk(1);
+    // updatePlanet.name = "terra";
+    // await updatePlanet.save();
+
+    const deletePlanet = await Planet.findByPk(newPlanet.id);
+    deletePlanet.destroy();
+
+    const updatePlanet = await Planet.findAll();
+
+    console.log(updatePlanet.map((p) => p.dataValues));
 })();
