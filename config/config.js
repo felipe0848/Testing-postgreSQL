b75cd71f544011ef2c86f56a63422d13326df10d
Felipe Soares <felipe.11.11@hotmail.com>
2023-07-19 10:39:05 -0400

    Create database

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "postgres",
+  password: "felipe123",
+  database: "testando_sequelize",
+  define: {
+    timestamps: true,
+  },
+};
