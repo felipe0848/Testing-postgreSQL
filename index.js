92d033848e4158bfc307ca92c508f13c477a06f5
Felipe Soares <felipe.11.11@hotmail.com>
2023-07-19 12:15:25 -0400

    Update Planet

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 (async () => {
-  const Planet = require("./models/Planet");
+    const Planet = require("./models/Planet");
 
-  //   const newPlanet = await Planet.create({
-  //     name: "Venus",
-  //     position: 5,
-  //   });
+    //   const newPlanet = await Planet.create({
+    //     name: "Venus",
+    //     position: 5,
+    //   });
 
-  const seePlanet = await Planet.findAll({
-    where: {
-      position: 3,
-    },
-  });
+    // const seePlanet = await Planet.findAll({
+    // where: {
+    //     position: 3,
+    // },
+    // });
 
-  console.log(seePlanet);
+    const updatePlanet = await Planet.findByPk(1);
+    updatePlanet.name = "terra";
+    await updatePlanet.save();
+
+    console.log(updatePlanet);
 })();
