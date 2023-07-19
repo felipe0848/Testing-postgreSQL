3a4f80164b98a29d4a1b9ba45eba18845ee4d944
Felipe Soares <felipe.11.11@hotmail.com>
2023-07-19 13:58:54 -0400

    Create route GET

--- Controller/PlanetController.js
+++ Controller/PlanetController.js
@@ -8,4 +8,8 @@ module.exports = {
 
         return res.json(newPlanet);
     },
+    async index(req, res) {
+        const planets = await Planet.findAll();
+        return res.json(planets);
+    },
 };
