01698e6293336ffb83e213036f25b9c8bdef1aaa
Felipe Soares <felipe.11.11@hotmail.com>
2023-07-20 10:20:55 -0400

    Create Migration Captain

---
+++ migrations/20230720141452-captain.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable("captain", {
+            id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            name: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            fromPlanet: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            createdAt: {
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                type: Sequelize.DATE,
+            },
+        });
+    },
+
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable("captain");
+    },
+};
