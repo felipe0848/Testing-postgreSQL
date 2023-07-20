b2d1a7ce4eaa784f9e2a652719f3e8eab12873cc
Felipe Soares <felipe.11.11@hotmail.com>
2023-07-20 10:45:50 -0400

    Fix Migrate capitains

--- migrations/20230720141452-captain.js
+++ migrations/20230720141452-captain.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
-        await queryInterface.createTable("captain", {
+        await queryInterface.createTable("captains", {
             id: {
                 type: Sequelize.INTEGER,
                 allowNull: false,
@@ -28,6 +28,6 @@ module.exports = {
     },
 
     async down(queryInterface, Sequelize) {
-        await queryInterface.dropTable("captain");
+        await queryInterface.dropTable("captains");
     },
 };
