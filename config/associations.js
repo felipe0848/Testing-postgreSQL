ba7397a9cb29918ca2c6e4d39928d143989aae35
Felipe Soares <felipe.11.11@hotmail.com>
2023-07-21 08:57:12 -0400

    Create associations

--- config/associations.js
+++ config/associations.js
@@ -1,7 +1,19 @@
+const Captain = require("../models/Captain");
+const Captain_spaceship = require("../models/Captain_spaceship");
 const Planet = require("../models/Planet");
 const Satelite = require("../models/Satelite");
+const Spaceship = require("../models/Spaceship");
 
 Planet.hasMany(Satelite, { onUpadate: "CASCADE", onDelete: "CASCADE" });
 Satelite.belongsTo(Planet, { foreingkey: "planetId", as: "planet" });
+
+Captain_spaceship.belongsToMany(Captain, {
+    foreingKey: "captainId",
+    as: "captains",
+});
+Captain_spaceship.belongsToMany(Spaceship, {
+    foreingKey: "spaceshipId",
+    as: "spaceships",
+});
 
 module.exports = { Planet, Satelite };
